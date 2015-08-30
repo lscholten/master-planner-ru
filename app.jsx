620879b515bcfaad2f5cc7da95e12d592347d97b
Luuk Scholten <info@luukscholten.com>
2015-08-30 15:36:38 +0200

    Requirements added and information science added

--- app.jsx
+++ app.jsx
@@ -23,49 +23,72 @@ var Application = React.createClass({
     addCourse: function (year, course) {
         year1 = this.state.courses.y1;
         year2 = this.state.courses.y2;
-        if (year == 1){
+        if (year == 1) {
             year1 = year1.concat([this.state.courseRepo[course]]);
         } else {
             year2 = year2.concat([this.state.courseRepo[course]]);
         }
-        this.setState({courses:{"y1": year1, "y2": year2}});
+        this.setState({courses: {"y1": year1, "y2": year2}});
+    },
+    removeCourse: function (coursecode) {
+        console.log(coursecode);
+        year1 = this.state.courses.y1.filter(function (course) {
+            return course.code != coursecode
+        });
+        year2 = this.state.courses.y2.filter(function (course) {
+            return course.code != coursecode
+        });
+        this.setState({courses: {"y1": year1, "y2": year2}});
+    },
+    getAllPickedCourses: function () {
+        return this.state.courses.y1.concat(this.state.courses.y2);
     },
     render: function () {
         return <div>
-            <SemesterList coursesPicked={this.state.courses} courseRepo={this.state.courseRepo}  />
-            <StudyProgramList programs={this.state.programRepo} repos={this.state} addCourse={ this.addCourse }/>
+            <SemesterList coursesPicked={this.state.courses} courseRepo={this.state.courseRepo}
+                          removeCourse={ this.removeCourse }/>
+            <StudyProgramList programs={this.state.programRepo} repos={this.state} addCourse={ this.addCourse }
+                              picked={this.getAllPickedCourses()}/>
         </div>;
     }
 });
 
 var SemesterList = React.createClass({
+    getSemesterCourses: function (year, semester) {
+        return this.props.coursesPicked[year].filter(function (el) {
+            return el.semester == semester;
+        }.bind(semester));
+    },
+    getTotalEcs: function () {
+        return this.props.coursesPicked.y1.reduce(function (carry, add) {
+                return carry + add.ec
+            }, 0) + this.props.coursesPicked.y2.reduce(function (carry, add) {
+                return carry + add.ec
+            }, 0);
+    },
     render: function () {
-        var y1s1 = this.props.coursesPicked.y1.filter(function(el) {
-            return el.semester == 1;
-        });
-        var y1s2 = this.props.coursesPicked.y1.filter(function(el) {
-            return el.semester == 2;
-        }) ;
-        var y2s1 = this.props.coursesPicked.y2.filter(function(el) {
-            return el.semester == 1;
-        });
-        var y2s2 = this.props.coursesPicked.y2.filter(function(el) {
-            return el.semester == 2;
-        });
+        var y1s1 = this.getSemesterCourses("y1", 1);
+        var y1s2 = this.getSemesterCourses("y1", 2);
+        var y2s1 = this.getSemesterCourses("y2", 1);
+        var y2s2 = this.getSemesterCourses("y2", 2);
 
         return <div className="row">
-            <Semester year={1} semester={1} courses={y1s1} courseRepo={this.props.courseRepo} />
-            <Semester year={1} semester={2} courses={y1s2} courseRepo={this.props.courseRepo} />
-            <Semester year={2} semester={1} courses={y2s1} courseRepo={this.props.courseRepo} />
-            <Semester year={2} semester={2} courses={y2s2} courseRepo={this.props.courseRepo} />
+            <div className="col-xs-12">
+                <h4>My master program</h4>
+                <em>Total: { this.getTotalEcs() } ec</em>
+            </div>
+            <Semester year={1} semester={1} courses={y1s1} removeCourse={this.props.removeCourse}/>
+            <Semester year={1} semester={2} courses={y1s2} removeCourse={this.props.removeCourse}/>
+            <Semester year={2} semester={1} courses={y2s1} removeCourse={this.props.removeCourse}/>
+            <Semester year={2} semester={2} courses={y2s2} removeCourse={this.props.removeCourse}/>
         </div>;
     }
 });
 
 var Semester = React.createClass({
     render: function () {
-        var totalEc = this.props.courses.reduce(function(total, el) {
-           return total += el.ec;
+        var totalEc = this.props.courses.reduce(function (total, el) {
+            return total + el.ec;
         }, 0);
 
         return <div className="col-xs-3">
@@ -75,7 +98,8 @@ var Semester = React.createClass({
                     <div className="badge pull-right">{ totalEc }</div>
                 </div>
                 { this.props.courses.map(function (course) {
-                    return <Course {...course} key={course.code} disableDragging={true} />
+                    return <Course {...course} key={course.code} removalMode={true}
+                                               removeCourse={this.props.removeCourse}/>
                 }.bind(this))
                 }
             </div>
@@ -88,7 +112,7 @@ var StudyProgramList = React.createClass({
         return <div>
             { this.props.programs.map(function (studyProgram) {
                 return <StudyProgram program={studyProgram} key={studyProgram.name} repos={this.props.repos}
-                                     addCourse={ this.props.addCourse }/>
+                                     addCourse={ this.props.addCourse } picked={this.props.picked}/>
             }.bind(this))
             }
         </div>;
@@ -97,16 +121,29 @@ var StudyProgramList = React.createClass({
 
 var StudyProgram = React.createClass({
     render: function () {
+        var totalEc = this.props.picked.reduce(function (total, el) {
+            return total + el.ec;
+        }, 0);
+
         return <div className="row">
             <div className="col-xs-12">
                 <h2>{ this.props.program.name }</h2>
             </div>
-            <CourseList name="Required courses" courses={ this.props.program.trackBasis }
-                        courseRepo={ this.props.repos.courseRepo } addCourse={ this.props.addCourse }/>
-            { this.props.program.trackElectiveGroups.map(function (group) {
-                return <CourseList name={group.groupName} courses={group.courses}
-                                   courseRepo={ this.props.repos.courseRepo} addCourse={ this.props.addCourse }/>
-            }.bind(this))
+            <div className="col-xs-12">
+                <b>Requirements</b> ({totalEc} / {this.props.program.ec} ec)<br />
+                {
+                    this.props.program.requirements.map(function (requirement, i) {
+                        return <Requirement {...requirement} program={this.props.program} picked={this.props.picked} key={i}/>
+                    }.bind(this))
+                }
+            </div>
+            {
+                Object.keys(this.props.program.courseGroups).map(function (key) {
+                    var group = this.props.program.courseGroups[key];
+                    return <CourseList key={key} name={group.name} courses={group.courses}
+                                       courseRepo={ this.props.repos.courseRepo} addCourse={ this.props.addCourse }
+                                       picked={ this.props.picked }/>
+                }.bind(this))
             }
         </div>;
     }
@@ -114,13 +151,21 @@ var StudyProgram = React.createClass({
 
 var CourseList = React.createClass({
     render: function () {
-        return <div className="col-xs-4">
+        return <div className="col-xs-3">
             <div className="panel panel-default">
                 <div className="panel-heading">{ this.props.name }</div>
                 { this.props.courses.map(function (course) {
                     var courseObj = this.props.courseRepo[course];
+                    var isPicked = false;
+                    for (var i = 0; i < this.props.picked.length; i++) {
+                        if (this.props.picked[i].code == courseObj.code) {
+                            isPicked = true;
+                            break;
+                        }
+                    }
 
-                    return <Course {...courseObj} key={course} addCourse={this.props.addCourse} courseCode={course} />
+                    return <Course {...courseObj} key={course} addCourse={this.props.addCourse} courseCode={course}
+                                                  isPicked={isPicked}/>
                 }.bind(this))
                 }
             </div>
@@ -129,41 +174,97 @@ var CourseList = React.createClass({
 });
 
 var Course = React.createClass({
-    componentDidMount: function () {
-        if (this.props.disableDragging != undefined) {
-            return;
-        }
-
+    setupDragging: function () {
         var container = React.findDOMNode(this);
         var dropYear1 = document.getElementById('year-1');
         var dropYear2 = document.getElementById('year-2');
+        var tmpthis = this;
         var drag = dragula([container, dropYear1, dropYear2], {
             copy: true,
+            moves: function (el, source, handle) {
+                return !tmpthis.props.isPicked;
+            },
             accepts: function (src, dest) {
-                return dest != container;
+                return dest != container && (dest == dropYear1 || dest == dropYear2);
             }
         });
 
-        drag.on('drop', function (drake, target, src) {
+        drag.on('drop', function (drake, target) {
             if (target == dropYear1) {
                 this.props.addCourse(1, this.props.courseCode);
-            } else {
+            } else if (target == dropYear2) {
                 this.props.addCourse(2, this.props.courseCode);
             }
             drake.remove();
         }.bind(this));
     },
+    componentDidMount: function () {
+        if (this.props.removalMode == undefined) {
+            this.setupDragging();
+        }
+    },
+    removeCourse: function (event) {
+        this.props.removeCourse(this.props.code);
+    },
     render: function () {
-        return <li className="list-group-item">
+        liClass = "list-group-item";
+        if (this.props.isPicked) {
+            liClass += " list-group-item-success";
+        }
+
+        return <li className={ liClass }>
             <div className="row">
-                <div className="col-xs-8">
-                    <b>{ this.props.name }</b> <br />
+                <div className="col-xs-9">
+                    <b>{ this.props.shortName != undefined ? this.props.shortName : this.props.name }</b> <br />
                     <em> Semester { this.props.semester } </em>
                 </div>
                 <div className="col-xs-3">
                     { this.props.ec } EC
+                    {
+                        (this.props.removalMode) ?
+                            <span className="glyphicon glyphicon-remove"
+                                  onClick={ this.removeCourse }></span>
+                            : ""
+                    }
                 </div>
             </div>
         </li>;
+    }
+});
+
+var Requirement = React.createClass({
+    calcFulfilledEcs: function () {
+        filteredPicked = this.props.picked.filter(function (course) {
+            for (var i = 0; i < this.props.courseGroups.length; i++) {
+                courses = this.props.program.courseGroups[this.props.courseGroups[i]].courses;
+                for (var j = 0; j < courses.length; j++) {
+                    if (courses[j] == course.code) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }.bind(this));
+
+        return filteredPicked.reduce(function (carry, elt) {
+            return carry + elt.ec;
+        }, 0);
+    },
+    render: function () {
+        currentEc = this.calcFulfilledEcs();
+
+        return <div className="row">
+            <div className="col-xs-3"><em>{ this.props.description }</em>&nbsp;</div>
+            <div className="col-xs-1">({currentEc}/{ this.props.ec} ec)</div>
+            <div className="col-xs-8">
+                Pick from: [
+                {
+                    this.props.courseGroups.map(function (cg) {
+                        return this.props.program.courseGroups[cg].name + ", ";
+                    }.bind(this))
+                }
+                ]
+            </div>
+        </div>
     }
 });
