41f7671949470f782695c7958a5628ed90e37236
Luuk Scholten <info@luukscholten.com>
2016-01-26 14:41:44 +0100

    Remove useless comments

--- app/actions/course-actions.js
+++ app/actions/course-actions.js
@@ -3,10 +3,6 @@ import jQuery from 'jquery';
 import AppDispatcher from '../core/app-dispatcher';
 import { CourseActionTypes } from '../stores/course-store';
 
-/**
- * Load all devices from API and dispatch event to CourseStore
- * API is a simple JSON file for now
- */
 export function loadCourses() {
     jQuery.ajax({
         url: "data/courses.json",
