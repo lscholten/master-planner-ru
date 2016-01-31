120f041c6d03c8936d43fe12d58c5098edb403b9
Luuk Scholten <info@luukscholten.com>
2016-01-31 12:15:51 +0100

    Stop requirement ecs from overflowing

--- app/components/requirement-list.jsx
+++ app/components/requirement-list.jsx
@@ -22,13 +22,55 @@ export default class RequirementList extends React.Component {
         this.setState({ chosenCourses: CourseStore.getAllChosen() });
     }
 
+    calculateEcsForRequirements() {
+        let assignedCourses = {};
+
+        this.props.program.requirements.forEach(requirement => {
+           assignedCourses[requirement.description] = 0;
+        });
+
+        let complexityRequirements = this.props.program.requirements.slice().sort((a, b) => b.courseGroups.length - a.courseGroups.length);
+
+        for (let course of this.state.chosenCourses) {
+            for (let req of complexityRequirements) {
+                if (req.courseGroups.length == 0) {
+                    assignedCourses[req.description] += course.ec;
+                    break;
+                }
+
+                if (assignedCourses[req.description] + course.ec > req.ec) {
+                    continue;
+                }
+
+                let assigned = false;
+
+                for (let courseGroup of req.courseGroups) {
+                    let courses = this.props.program.courseGroups[courseGroup].courses;
+                    for (let grpCourse of courses) {
+                        if (grpCourse == course.code) {
+                            assignedCourses[req.description] += course.ec;
+                            assigned = true;
+                        }
+                    }
+                }
+
+                if (assigned) {
+                    break;
+                }
+            }
+        }
+
+        return assignedCourses;
+    }
+
     render() {
+        let requirementEcs = this.calculateEcsForRequirements();
         return (
             <div className="col-xs-12">
                 <b>Requirements</b> ({ CourseStore.getTotalEcs() } / { this.props.program.ec } ec)<br />
                 {
                     this.props.program.requirements.map(function (requirement, i) {
-                        return <Requirement {...requirement} program={ this.props.program } key={i} chosenCourses={ this.state.chosenCourses } />
+                        return <Requirement {...requirement} program={ this.props.program } key={i} fulfilled={ requirementEcs[requirement.description] } />
                     }.bind(this))
                 }
             </div>
