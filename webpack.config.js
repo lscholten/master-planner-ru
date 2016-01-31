a720ca4eb8404af8f7c8a8af1bf245cce5091064
Luuk Scholten <info@luukscholten.com>
2016-01-31 13:56:03 +0100

    Optimize build script and add entry point

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ module.exports = {
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
   ]
 };
